c16af701c6a6bff73fe2991db072e4bf1a951f43
LegiOnGH <rohitthakur438a@gmail.com>
2026-01-22 16:22:58 +0530

    Spring Boot Journal App - Logging

--- src/main/java/com/legion/journalApp/service/JournalEntryService.java
+++ src/main/java/com/legion/journalApp/service/JournalEntryService.java
@@ -4,16 +4,19 @@ package com.legion.journalApp.service;
 import com.legion.journalApp.entity.JournalEntry;
 import com.legion.journalApp.entity.User;
 import com.legion.journalApp.repository.JournalEntryRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
 
 @Service
+@Slf4j
 public class JournalEntryService {
 
     @Autowired
@@ -53,8 +56,14 @@ public class JournalEntryService {
                 userService.saveUser(user);
                 journalEntryRepo.deleteById(id);
             }
+            log.info("haha you noob");
+            log.warn("haha you noob");
+            log.error("haha you noob");
+            log.debug("haha you noob"); //won't come unless enabled
+            log.trace("haha you noob"); //won't come unless enabled
+
+
         }catch(Exception e){
-            System.out.println(e);
             throw new RuntimeException("Error occurred while deleting journal entry : ", e);
         }
         return removed;
